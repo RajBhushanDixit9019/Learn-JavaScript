94893bf65e9c557ffcb52a0e02ee81213a2a69ae
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2024-02-07 02:39:10 +0530

    First commit

---
+++ variables.js
@@ -0,0 +1,11 @@
+// this javascript code illustrate about the variables...
+
+// console.log(); is used to print anything on string..
+console.log("Running javascript variables.");
+console.log("Variales are the storage containers used to store the data.");
+
+// using variables in javascript...
+console.log("This is how the variables in javascript are used.");
+
+console.log("Before using 2 steps are important are:");
+console.log("1st declareation and 2nd assignment.");
