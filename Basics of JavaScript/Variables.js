081abbaa9c9f47e634623ca482d2dc2da7ce88b2
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2025-01-05 14:43:34 +0530

    Variables
    Learnt about the major 3 types of variables in JavaScript:
    1. let
    2. var
    3. const

---
+++ Basics of JavaScript/Variables.js
@@ -0,0 +1,38 @@
+// this JavaScript code is to illustrate
+// the concept of variables in JS
+
+// There are four kind of varibles in JS:
+// 1. var (global Scoped)
+// 2. let (local scoped)
+// 3. const (constent variable)
+
+// 4. val(old)
+
+// Variables are the container to store the values in the memory
+
+// const variable 
+const accoutId = 123
+
+// let variable
+let accountEmail = "raj@example.com"
+
+// var variable
+var  accountPassword = "12345"
+
+// black
+accountCity = "Banglore"
+
+//accoutId = 13 not allowd
+
+
+accountEmail = "raj@gmail.com"
+accountPassword = 2121
+accountCity = "Jaipur"
+
+console.table([accoutId, accountEmail, accountPassword,accountCity])
+
+let accountState;
+console.log(accountState); // undefined
+
+
+
